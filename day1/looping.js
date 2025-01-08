b7f53a45b3f554d180e49c16867dfcdd81f1a079
Himabindured <reddyphimabindu@gmail.com>
2025-01-08 21:42:55 +0530

    assignment2

---
+++ day1/looping.js
@@ -0,0 +1,20 @@
+//for(let i=1;i<=100;i++){
+  
+//if(i % 2 == 0){
+   // console.log( i," is a even number");
+//}else{
+   // console.log(i,"is a odd number");
+//}}
+   //let n=5;
+    //let x = 1;
+  
+   // for(let i =1;i <=n ;i++){
+        //x = x * i;
+        
+   // }
+  // console.log(x);
+  function addToCart() {
+   alert("Product added!");
+   document.body.style.background = "blue";
+}
+
